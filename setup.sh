50e857af00b89371782672e38c20638acfcaf3d6
Caysen Mayo <91717010+lumen-novum@users.noreply.github.com>
2024-08-20 15:57:57 -0500

    Changed dependencies and automated start script preparation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 # Download package
 wget https://github.com/yuezk/GlobalProtect-openconnect/releases/download/v2.3.7/globalprotect-openconnect_2.3.7-1_arm64.deb
 
-# Then install the dependencies and pacakge
-sudo apt install gir1.2-gtk-3.0 gir1.2-webkit2-4.0 ./globalprotect-openconnect_2.3.7-1_arm64.deb
+# Then install git (for easy updating) and the pacakge
+sudo apt install git ./globalprotect-openconnect_2.3.7-1_arm64.deb
+
+# Remove installed .deb file
+rm globalprotect-openconnect_2.3.7-1_arm64.deb
+
+# Copy starting script to desktop
+cp start.sh $HOME/Desktop/start-vpn
+
+# Allow file to be executed from desktop without CLI
+chmod +x $HOME/Desktop/start-vpn
