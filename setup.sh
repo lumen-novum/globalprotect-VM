9ff086a2fbe93487b4814da1af4e193024e3f12c
Caysen Mayo <91717010+lumen-novum@users.noreply.github.com>
2024-08-20 15:39:02 -0500

    Removing unnecessary package

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 wget https://github.com/yuezk/GlobalProtect-openconnect/releases/download/v2.3.7/globalprotect-openconnect_2.3.7-1_arm64.deb
 
 # Then install the dependencies and pacakge
-sudo apt install git gir1.2-gtk-3.0 gir1.2-webkit2-4.0 ./globalprotect-openconnect_2.3.7-1_arm64.deb
+sudo apt install gir1.2-gtk-3.0 gir1.2-webkit2-4.0 ./globalprotect-openconnect_2.3.7-1_arm64.deb
