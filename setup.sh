5f5f7af7a939cadc2cfdc79023b880d541a609a3
lumen-novum <novablackxd@gmail.com>
2024-08-20 12:54:57 -0500

    Added basic installation script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# Download package
+wget https://github.com/yuezk/GlobalProtect-openconnect/releases/download/v2.3.7/globalprotect-openconnect_2.3.7-1_arm64.deb
+
+# Then install the dependencies and pacakge
+sudo apt install git gir1.2-gtk-3.0 gir1.2-webkit2-4.0 ./globalprotect-openconnect_2.3.7-1_arm64.deb
