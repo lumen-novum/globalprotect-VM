c6b961e7bbd54aef5a81278fd8f99acccb043d74
Caysen Mayo <91717010+lumen-novum@users.noreply.github.com>
2024-08-20 17:44:10 -0500

    Remove unneeded git

--- setup.sh
+++ setup.sh
@@ -1,14 +1,8 @@
 # Download package
 wget https://github.com/yuezk/GlobalProtect-openconnect/releases/download/v2.3.7/globalprotect-openconnect_2.3.7-1_arm64.deb
 
-# Then install git (for easy updating) and the pacakge
-sudo apt install git ./globalprotect-openconnect_2.3.7-1_arm64.deb
-
-# Create local repository
-git init -b main
-
-# Set up remote to pull from
-git remote add origin https://github.com/lumen-novum/globalprotect-VM.git
+# Then install the pacakge
+sudo apt install ./globalprotect-openconnect_2.3.7-1_arm64.deb
 
 # Remove installed .deb file
 rm globalprotect-openconnect_2.3.7-1_arm64.deb
