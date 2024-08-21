8cc38f1139b4b89595fe91c78a599e42cf7101af
Caysen Mayo <91717010+lumen-novum@users.noreply.github.com>
2024-08-20 19:00:02 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-su caysen -c "gpclient connect --browser firefox utdvpn.utdallas.edu"
+su caysen 
+gpclient connect --browser firefox utdvpn.utdallas.edu
 #xfce4-terminal -e 'bash -c "sleep 30"'
