1d317786e4928e9a4f8582e6002082aa4fe5b455
Caysen Mayo <91717010+lumen-novum@users.noreply.github.com>
2024-08-20 19:15:35 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #su caysen -c "xfce4-terminal -e 'gpclient connect --browser default utdvpn.utdallas.edu'"
-su caysen -c "gpclient connect --browser default utdvpn.utdallas.edu" >& /home/caysen/log.txt
+su caysen -c "gpclient connect --browser default utdvpn.utdallas.edu" 2> /home/caysen/log.txt
