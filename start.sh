78760902804f58f124fb35f87029b10ca91ec0f1
Caysen Mayo <91717010+lumen-novum@users.noreply.github.com>
2024-08-20 19:27:12 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-#su caysen -c "xfce4-terminal -e 'gpclient connect --browser default utdvpn.utdallas.edu'"
-xfce4-terminal -e 'gpclient connect --browser default utdvpn.utdallas.edu'
-#gpclient connect --browser default utdvpn.utdallas.edu
+gpclient connect --browser default utdvpn.utdallas.edu
