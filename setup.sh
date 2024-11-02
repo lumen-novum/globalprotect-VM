2693f99f13fc85c88d62717b380eda7398094934
Caysen Mayo <91717010+lumen-novum@users.noreply.github.com>
2024-11-02 17:47:04 -0500

    updating VPN

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 # Download package
-wget https://github.com/yuezk/GlobalProtect-openconnect/releases/download/v2.3.7/globalprotect-openconnect_2.3.7-1_arm64.deb
+wget https://github.com/yuezk/GlobalProtect-openconnect/releases/download/v2.3.9/globalprotect-openconnect_2.3.9-1_arm64.deb
 
 # Then install the pacakge
-sudo apt install ./globalprotect-openconnect_2.3.7-1_arm64.deb
+sudo apt install ./globalprotect-openconnect_2.3.9-1_arm64.deb
 
 # Remove installed .deb file
-rm globalprotect-openconnect_2.3.7-1_arm64.deb
+rm globalprotect-openconnect_2.3.9-1_arm64.deb
 
 # Copy startup script to bin
 sudo cp start.sh /usr/local/bin/start-vpn
